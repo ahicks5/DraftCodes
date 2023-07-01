1cb37b01acf46f76633d2d10b33fca924876eead
arhic <ahicks5.nd@gmail.com>
2023-07-01 01:36:15 -0500

    cleanup start bov

--- clean_files.py
+++ clean_files.py
@@ -2,19 +2,18 @@ from connectSources import find_ref_dfs
 import pandas as pd
 from datetime import datetime
 import pytz
-
-team_ref_df, sport_ref_df, espn_df, bovada_df = find_ref_dfs()
-
+from loadCSVs import load_previous_data
 try:
-    ind_df = pd.read_csv('/var/www/html/Website/Indicator_Data.csv')
+    BOVADA_FILEPATH = '/var/www/html/Website/' + 'Bovada_Data.csv'
+    ESPN_FILEPATH = '/var/www/html/Website/' + 'ESPN_Data.csv'
 except:
-    ind_df = pd.read_csv('Indicator_Data.csv')
+    BOVADA_FILEPATH = 'Bovada_Data.csv'
+    ESPN_FILEPATH = 'ESPN_Data.csv'
 
 class cleanFiles:
     def __init__(self):
-        self.espn_df = espn_df
-        self.bovada_df = bovada_df
-        self.ind_df = ind_df
+        self.espn_df = load_previous_data()['espn_data']
+        self.bovada_df = load_previous_data()['bovada_data']
 
     def clean_all(self):
         # Convert the strings in 'game_time' to datetime objects
@@ -23,9 +22,10 @@ class cleanFiles:
         new_espn = self.espn_df[self.espn_df['game_time'] >= today]
 
         try:
-            new_espn.to_csv('/var/www/html/Website/ESPN_Data.csv', index=False)
+            new_espn.to_csv('/var/www/html/Website/' + 'ESPN_Data.csv', index=False)
         except:
             new_espn.to_csv('ESPN_Data.csv', index=False)
+
 
         # Convert the strings in 'game_startTime_cst' to datetime objects
         self.bovada_df['game_startTime_cst'] = pd.to_datetime(self.bovada_df['game_startTime_cst'],
@@ -34,12 +34,14 @@ class cleanFiles:
         cdt = pytz.timezone('America/Chicago')
         today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
         today = cdt.localize(today)
-        new_bov = self.bovada_df[self.bovada_df['game_startTime_cst'] >= today]
 
+        new_bov = self.bovada_df[self.bovada_df['game_startTime_cst'] >= today]
         try:
-            new_bov.to_csv('/var/www/html/Website/Bovada_Data.csv', index=False)
+            new_bov.to_csv('/var/www/html/Website/' + 'Bovada_Data.csv', index=False)
         except:
             new_bov.to_csv('Bovada_Data.csv', index=False)
+
+        print('All clean!')
 
 
 if __name__ == '__main__':
